c948d952e76ddb4407013a936322a0a0f1a2be24
kayeli dennis <kdenno25@gmail.com>
2021-06-30 17:15:37 +0300

    app router tests

--- NASAImagesViewerTests/AppRouterTests.swift
+++ NASAImagesViewerTests/AppRouterTests.swift
@@ -7,8 +7,50 @@
 
 import Foundation
 import XCTest
-
 @testable import NASAImagesViewer
 
-class ApprouterTests: XCTest {
+class ApprouterTests: XCTestCase {
+    let sut = AppRouter.shared
+    var item: Datum?
+    
+    override func setUp() {
+        super.setUp()
+        item = .init(keywords: nil,
+                     mediaType: .image,
+                     nasaID: "",
+                     dateCreated: Date(),
+                     datumDescription: "",
+                     center: Center.hq,
+                     title: "",
+                     photographer: nil,
+                     description508: "",
+                     secondaryCreator: "",
+                     location: "",
+                     album: nil)
+    }
+
+    override func tearDown() {
+        item = nil
+        super.tearDown()
+    }
+
+    func test_root_view_controller_is_HomeViewcontroller() {
+        guard let nav = sut.rootController() as? UINavigationController else {
+            return XCTFail("Failed to grab the navigation controller on the router object")
+        }
+        XCTAssert(nav.viewControllers.first is HomeViewController)
+    }
+    
+    func test_routes_to_image_detail_view_controller() {
+        guard let item = item else {
+            return XCTFail("Failed to get the datum")
+        }
+        
+        guard let nav = sut.rootController() as? UINavigationController else {
+            return XCTFail("Failed to grab the navigation controller on the routeer object")
+        }
+        
+        sut.presentImageDetailViewController(for: item, imageURL: URL(string: "test_url")!)
+        XCTAssert(nav.viewControllers.last is ImageDetailViewController)
+    }
 }
