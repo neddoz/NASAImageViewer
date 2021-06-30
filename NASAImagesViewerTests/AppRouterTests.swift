3b8a42c94a2a688499834863645097c993851060
kayeli dennis <kdenno25@gmail.com>
2021-06-30 14:02:33 +0300

    starting out on unit tests

---
+++ NASAImagesViewerTests/AppRouterTests.swift
@@ -0,0 +1,14 @@
+//
+//  AppRouterTests.swift
+//  NASAImagesViewerTests
+//
+//  Created by kayeli dennis on 30/06/2021.
+//
+
+import Foundation
+import XCTest
+
+@testable import NASAImagesViewer
+
+class ApprouterTests: XCTest {
+}
